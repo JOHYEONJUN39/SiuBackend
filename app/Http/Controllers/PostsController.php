8c8127c2d4a84597cbca3059f4a092fc8e7ed4a4
Ydaehan <daehan821@naver.com>
2023-09-12 12:44:46 +0900

    ✨ feat :  특정 Id조회 시 조회수 증가 구현

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -78,6 +78,11 @@ class PostsController extends Controller
             // 게시글이 존재하지 않을 경우 404 에러 반환 또는 다른 처리
             return response()->json(['message' => '게시글을 찾을 수 없습니다.'], 404);
         }
+        // 사용자 정보 넘겨주기 (미구현)
+
+        // 조회수 증가
+        $post->view++;
+        $post->save();
 
         // 응답을 Json으로 생성
         return response()->json(['post' => $post]);
