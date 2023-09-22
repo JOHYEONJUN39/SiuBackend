1c4e71c44d76e2144a4292f9dd323c70ba70fb08
Ydaehan <daehan821@naver.com>
2023-09-22 12:33:39 +0900

    🔨 chore : 검색기능 결과 간략화 및 이미지 업로드 오류 수정

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\User;
 use App\Helpers\ImageHelper;
 use App\Models\PostImage;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -93,26 +95,34 @@ class PostController extends Controller
     public function retrievePostId($id){
         // 요청된 ID에 해당하는 게시글 탐색
         $post = Post::find($id);
-        $postImage = PostImage::where('post_id',$id)->get();
 
         if (!$post) {
             // 게시글이 존재하지 않을 경우 404 에러 반환 또는 다른 처리
             return response()->json(['message' => '게시글을 찾을 수 없습니다.'], 404);
         }
         // 사용자 정보 넘겨주기
-        
+        $userId = $post->user_id;
+        $userData = User::find($userId);
+
+        $tagList =  $post->tags;
+        $postData = $post->only(
+            'id',
+            'title',
+            'article',
+            'view', 
+            'user_id', 
+            'created_at', 
+            'updated_at'
+        );
+        $tags = $tagList->pluck('tag_name');
+            
         // 조회수 증가
         $post->view++;
         $post->save();
 
-        $data = [
-            $post,
-            $postImage,
-        ];
-
         // 응답을 Json으로 생성
-        return response()->json($data);
-    }
+        return response()->json(["post" => $postData, "user" => $userData, "tags" => $tags]);
+    } 
 
     // 특정 태그로 조회
     public function retrievePostTagId(Request $request){
@@ -128,27 +138,28 @@ class PostController extends Controller
         // 해당 태그와 연결된 게시글을 가져옴
         $posts = $tag->posts;
 
-        // 해당 게시글과 연관된 모든 태그들을 가져옴
-        $relatedTags = $tag->posts->flatMap(function ($post) {
-            return $post->tags->pluck('tag_name');
+        // 게시물이 가지고 있는 태그를 한번에 보여주기 위한 join
+        $searchTag = DB::table('posts')
+        ->join('post_tags', 'posts.id', '=', 'post_tags.post_id')
+        ->join('tags', 'post_tags.tag_id', '=', 'tags.id')
+        ->select('post_id', 'title', 'article', 'view', 'user_id', 'posts.created_at', 'posts.updated_at', DB::raw('GROUP_CONCAT(tag_name) as tag_names'))
+        ->groupBy('post_id')
+        ->havingRaw('GROUP_CONCAT(tag_name) LIKE ?', ["%$tagName%"])
+        ->get();
+
+        // 문자열에서 "[\]" 제거
+        $searchTag = $searchTag->map(function ($item) {
+            $tagNames = explode(',', $item->tag_names);
+            $tagNames = array_map(function ($tagName) {
+                return trim($tagName, '" ');
+            }, $tagNames);
+            $item->tag_names = $tagNames;
+            return $item;
         });
 
-        // json 반환 방식 설정
-        $data = [
-            'posts' => $posts->map(function($post) {
-                return [
-                    'id' => $post->id,
-                    'title' => $post->title,
-                    'article' => $post->article,
-                    'view' => $post->view,
-                    'created_at' => $post->created_at,
-                    'updated_at' => $post->updated_at,
-                ];
-            }),
-            'relatedTags' => $relatedTags
-        ];
+        
 
-        return response()->json([$data]);
+        return response()->json($searchTag);
         // 태그 이름값도 보내 주어야 함
     }
 
@@ -236,7 +247,7 @@ class PostController extends Controller
                      ->orWhere('article', 'like', "$search%")
                      ->get();
 
-        return response()->json(['posts' => $posts]);
+        return response()->json($posts);
     }
 
     // 제목+내용 일치 검색
@@ -247,23 +258,22 @@ class PostController extends Controller
                      ->get();
 
         
-        $data = [
-            'posts' => $posts->map(function ($post) {
-                return [
-                    'id' => $post->id,
-                    'title' => $post->title,
-                    'article' => $post->article,
-                    'view' => $post->view,
-                    'user_id' => $post->user_id,
-                    'created_at' => $post->created_at,
-                    'updated_at' => $post->updated_at,
-                    'tag_name' => $post->tags->pluck('tag_name'),
-                    ];
-                })
-            ];             
+        $data = $posts->map(function ($post) {
+                    return [
+                        'id' => $post->id,
+                        'title' => $post->title,
+                        'article' => $post->article,
+                        'view' => $post->view,
+                        'user_id' => $post->user_id,
+                        'created_at' => $post->created_at,
+                        'updated_at' => $post->updated_at,
+                        'tag_name' => $post->tags->pluck('tag_name'),
+                        ];
+                    });
+                         
         
 
-        return response()->json([$data]);
+        return response()->json($data);
     }
 
     // 연관 태그 검색
