75a2daa6e8a73c77d226cbe796c637c6bc60661a
Hyn2 <justin010129@gmail.com>
2023-09-25 17:08:09 +0900

    ✨ feat : Add logic for signout user

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -11,6 +11,7 @@ class LogoutController extends Controller
     public function __invoke(Request $request) {
         Auth::logout();
         $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return response()->json(['message' => 'logged out successfully']);
     }
 }
